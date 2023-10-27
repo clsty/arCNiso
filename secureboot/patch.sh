e16abc4f0facfdc8afba57180320dd1cf5c3dc5b
clsty <celestial.y@outlook.com>
2023-10-27 10:52:22 +0800

    Secure Boot support.

---
+++ secureboot/patch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $(dirname $0)
+cp -f /usr/bin/mkarchiso .
+patch -p0 -i mkarchiso.patch
+echo "补丁已打，但可能失败（看上面的提示）并生成 mkarchiso.rej 文件。"
+echo "若有失败部分，请一边阅读 ./mkarchiso.rej，一边手动编辑 ./mkarchiso。"
+echo "直到手动打完补丁之后，为了下次更好合并，建议在当前目录运行 ./diff.sh 以生成新的补丁。"
