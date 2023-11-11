e5209a0ce9df88395ccce96ebb30fadaaea773b0
clsty <celestial.y@outlook.com>
2023-11-11 18:08:18 +0800

    minor fixes.

--- anotherpac/prepare-icon-git.sh
+++ anotherpac/prepare-icon-git.sh
@@ -7,11 +7,10 @@ cd "$(dirname $selfpath)"
 basedir="$(pwd)"
 target="$basedir"/cache/icons
 
-ping -c1 github.com||if true;then echo "请确保能够连接到网络。";exit 1;fi
 echo "!! 现在将准备 icons（使用 git），请确保网络通畅。"
 read -p "按回车继续：" tmpvar
 
-cd $target
+mkdir -p $target;cd $target
 git clone https://github.com/clsty/papirus-icon-theme||(cd papirus-icon-theme&&git pull)
 
 echo "icons 准备流程完毕。"
