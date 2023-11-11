1781bde09676272185ae80b5b142d9645e890bca
clsty <celestial.y@outlook.com>
2023-11-11 18:02:18 +0800

    Minor fixes.

--- anotherpac/prepare-icon-svn.sh
+++ anotherpac/prepare-icon-svn.sh
@@ -6,7 +6,7 @@ function try { "$@" || sleep 0; }
 selfpath="${BASH_SOURCE[0]}"
 cd "$(dirname $selfpath)"
 basedir="$(pwd)"
-target="$basedir"/cache/papirus-icons
+target="$basedir"/cache/papirus-icon-theme
 tmpdir="/tmp/anotherpac/icons"
 
 rm -rf "$tmpdir"
