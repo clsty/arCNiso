25ec00bb0aed9612a05f50669508ff6e9043378c
clsty <celestial.y@outlook.com>
2023-11-28 09:29:30 +0800

    minor fix.

--- homebase/preparefirefox.sh
+++ homebase/preparefirefox.sh
@@ -47,10 +47,7 @@ do rm -rf "$target"/"$i";done
 cleanff
 
 which firefox || sudo pacman -S --needed firefox
-ping -c1 archlinuxcn.org || if true; then
-	echo "请确保能够连接到网络。"
-	exit 1
-fi
+ping -c1 archlinuxcn.org || echo "[警告]网络似乎未连接。"
 echo "!! 现在将准备 Firefox 的配置目录，请确保网络通畅。"
 echo "Firefox 将被运行。在此期间，请自行配置插件等。完毕之后，请自行将其退出（建议在退出前，设置>隐私与安全>Cookie 和网站数据>清除数据，以及历史记录>清除历史记录）。"
 read -p "按回车继续：" tmpvar
