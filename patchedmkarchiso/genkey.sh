a8a7a55ff24c8da6ae2fd10aa47e368635b6e8d5
clsty <celestial.y@outlook.com>
2023-12-05 21:32:57 +0800

    Minor update.

--- patchedmkarchiso/genkey.sh
+++ patchedmkarchiso/genkey.sh
@@ -6,20 +6,21 @@ cd $(dirname $0)
 # Create a GUID for owner identification
 uuidgen --random >GUID.txt
 
-# Platform key
+# Platform key (PK)
 openssl req -newkey rsa:4096 -nodes -keyout PK.key -new -x509 -sha256 -days 3650 -subj "/CN=arCNiso Platform Key/" -out PK.crt
 openssl x509 -outform DER -in PK.crt -out PK.cer
 cert-to-efi-sig-list -g "$(<GUID.txt)" PK.crt PK.esl
 sign-efi-sig-list -g "$(<GUID.txt)" -k PK.key -c PK.crt PK PK.esl PK.auth
+# Sign an empty file to allow removing Platform Key when in "User Mode":
 sign-efi-sig-list -g "$(<GUID.txt)" -c PK.crt -k PK.key PK /dev/null noPK.auth
 
-# Key Exchange Key
+# Key Exchange Key (KEK)
 openssl req -newkey rsa:4096 -nodes -keyout KEK.key -new -x509 -sha256 -days 3650 -subj "/CN=arCNiso Key Exchange Key/" -out KEK.crt
 openssl x509 -outform DER -in KEK.crt -out KEK.cer
 cert-to-efi-sig-list -g "$(<GUID.txt)" KEK.crt KEK.esl
 sign-efi-sig-list -g "$(<GUID.txt)" -k PK.key -c PK.crt KEK KEK.esl KEK.auth
 
-# Signature Database key
+# Signature Database key (db)
 openssl req -newkey rsa:4096 -nodes -keyout db.key -new -x509 -sha256 -days 3650 -subj "/CN=arCNiso Signature Database key/" -out db.crt
 openssl x509 -outform DER -in db.crt -out db.cer
 cert-to-efi-sig-list -g "$(<GUID.txt)" db.crt db.esl
